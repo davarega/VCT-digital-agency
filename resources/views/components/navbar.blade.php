cecc907e7302775ff59d975f80cff66928890837
davarega <xlithgt12@gmail.com>
2025-12-08 18:43:15 +0700

    new file:   app/View/Components/layout.php 	new file:   app/View/Components/navbar.php 	new file:   package-lock.json 	modified:   package.json 	new file:   public/image/header.jpg 	new file:   public/image/icon.png 	new file:   resources/views/components/layout.blade.php 	new file:   resources/views/components/navbar.blade.php 	modified:   resources/views/welcome.blade.php 	modified:   tailwind.config.js 	modified:   vite.config.js

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,28 @@
+<nav>
+    <div class="container mx-auto flex items-center justify-between h-16">
+        <img src={{ asset('image/icon.png') }} alt="icon" class="w-12">
+        <div class="flex divide-x-2 divide-dashed divide-gray-300 hidden md:flex">
+            <div class="px-6">
+                <p class="flex flex-col font-bold text-xl"><span class="text-xs text-gray-400">vct-agency@gmail.com</span>Mail to us</p>
+            </div>
+            <div class="px-6">
+                <p class="flex flex-col font-bold text-xl"><span class="text-xs text-gray-400">telepon</span>(+62)123456789</p>
+            </div>
+            <div class="px-6">
+                <p class="flex flex-col font-bold text-xl"><span class="text-xs text-gray-400">vct-agency@gmail.com</span>Mail to us</p>
+            </div>
+            <div class="px-6">
+                <p class="flex flex-col font-bold text-xl"><span class="text-xs text-gray-400">vct-agency@gmail.com</span>Mail to us</p>
+            </div>
+        </div>
+    </div>
+    <hr class="border-gray-300/50 my-1">
+    <div class="container flex mx-auto max-h-16">
+        <div class="flex">
+            <a href="#" class="font-semibold text-xl mx-3 px-5 py-3">Home</a>
+            <a href="#" class="font-semibold text-xl mx-3 px-5 py-3">About Us</a>
+            <a href="#" class="font-semibold text-xl mx-3 px-5 py-3">Services</a>
+            <a href="#" class="font-semibold text-xl mx-3 px-5 py-3">Testimoni</a>
+        </div>
+    </div>
+</nav>
